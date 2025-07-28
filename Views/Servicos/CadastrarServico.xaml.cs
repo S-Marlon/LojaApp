41b768b1b6e5f3c3cdea4e8f236e26509246b0fd
S-Marlon <marlovcbol.com.br@gmail.com>
2025-07-28 11:58:51 -0300

    Estilização da pagina de serviços

--- Views/Servicos/CadastrarServico.xaml.cs
+++ Views/Servicos/CadastrarServico.xaml.cs
@@ -5,7 +5,8 @@ public partial class CadastrarServico : ContentPage
 	public CadastrarServico()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new ServicoViewModel();
+    }
 
     private void inicio(object sender, EventArgs e)
     {
---
+++ Views/Servicos/ServicoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using LojaApp.Views.Servicos;
+
+public class ServicoViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<ServicoModel> Servicos { get; set; }
+
+    public ServicoViewModel()
+    {
+        Servicos = new ObservableCollection<ServicoModel>
+        {
+            new ServicoModel { NomeCliente = "João Silva", TipoServico = "Instalação de Bomba", Data = "25/07/2025", Garantia = "na garantia", Codigoservico = "34567"},
+            new ServicoModel { NomeCliente = "Maria Oliveira", TipoServico = "Limpeza de Poço", Data = "23/07/2025", Garantia = "na garantia", Codigoservico = "34545667" },
+            new ServicoModel { NomeCliente = "Pedro Santos", TipoServico = "Reparo Elétrico", Data = "20/07/2025", Garantia = "Fora da garantia", Codigoservico = "3457"  },
+        };
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
